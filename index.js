c5b0bbb1374811d94d99885a2b3013e0e1865e28
Dharaneesh <dharaneeshr8888@gmail.com>
2026-04-22 17:25:22 +0530

    add app files and package.json

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const client = require('prom-client');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from Microservice 🚀');
+// Default metrics (CPU, memory, etc.)
+client.collectDefaultMetrics();
+
+// Custom metric
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of requests'
 });
 
-app.listen(3000, () => {
-  console.log('Microservice running on port 3000');
+app.get('/', (req, res) => {
+  httpRequestCounter.inc();
+  res.send('Hello from Microservice');
+});
+
+// Prometheus endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
